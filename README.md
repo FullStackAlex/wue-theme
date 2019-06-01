740f67f0e046a4e2f953e78a95d3becc394bc485
Alex Podlubnyj <alex@tech-nomad.de>
2019-06-01 07:59:19 +0200

    Fuck that shit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Wue Theme - A Vue.js WordPress single page application starter theme  
+# Update 01.06.2019 
+# Sorry Guys, but SEO of this repo is currently not working properly. Google bots are unpredictable - all the time till yesterday the bots have indexed properly all the pages I have manually requested indexation for (from Google Search Console). And my site was even on the first page when searching for "vue wordpress theme seo". But today the Google bot didn't wait until Vue.js has rendered the content, even the page load is around 500ms.  And my site is not only out of the first search results (when googling for "vue wordpress theme seo") but is not available at all in the search results :sob: So to be on the safe side there is no getting around outputting bare HTML for the bots :(  [See official statetement by Google I've found today and which was updated last time in Mai 2019](https://developers.google.com/search/docs/guides/dynamic-rendering). That's what I will go for next weeks/months. Nuxt.js / Node.js in context of WordPress is still not an option to me.  
+
+# Here my previous blabla :grin:
+
+## Wue Theme - A Vue.js WordPress single page application starter theme  
 with all the goodness of both worlds: 
 - Webpacks Hot Module Replacement (HMR) inside php files! 
 - PHP rendering (keep using usual WordPress templates to organise your theme)! 
--- wordpress/wp-content/themes/wuetheme/inc/custom-endpoints/frontpage.php
+++ wordpress/wp-content/themes/wuetheme/inc/custom-endpoints/frontpage.php
@@ -26,48 +26,6 @@ function technomad_get_frontpage() {
 	$data["content"] = $content;
 	$data["title"]   = $title;
 
-
-	/**
-	 * some other custom frontpage data example (used  once for a project)
-	 **/
-	/*
-		$attachments = get_posts( array(
-			'post_type'   => 'attachment',
-			'numberposts' => - 1,
-			'status'      => 'publish',
-			'tax_query'   => array(
-				array(
-					'taxonomy' => 'attachment_category',
-					'field'    => 'slug',
-					'terms'    => 'featured-slider'
-				)
-			)
-		) );
-
-		foreach ( $attachments as $index => $attachment ) {
-			$data["featured-slider"][ $index ]["title"]     = $attachment->post_title;
-			$data["featured-slider"][ $index ]["ID"]        = $attachment->ID;
-			$data["featured-slider"][ $index ]["guid"]      = $attachment->guid;
-			$data["featured-slider"][ $index ]["thumbnail"] = wp_get_attachment_image( $attachment->ID, "large" );
-		}
-
-
-		$terms  = get_terms( [
-			'taxonomy'   => "attachment_category",
-			'hide_empty' => true,
-		] );
-		foreach ( $terms as $thema ) {
-			if ( $thema->slug !== "featured-slider" ) {
-				$data["themen"][ $thema->slug ]["slug"]           = $thema->slug;
-				$data["themen"][ $thema->slug ]["name"]           = $thema->name;
-				$data["themen"][ $thema->slug ]["count"]          = $thema->count;
-				$data["themen"][ $thema->slug ]["term_id"]        = $thema->term_id;
-				$featured_image                                   = get_field( "featured_image", $thema );
-				$data["themen"][ $thema->slug ]["featured_image"] = wp_get_attachment_image( $featured_image, "medium" );
-			}
-		}*/
-
-
 	return $data;
 
 }
